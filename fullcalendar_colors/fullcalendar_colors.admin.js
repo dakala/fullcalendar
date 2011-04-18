9b034b8efd73fe6f7a37ec4913798d0b3e45bfab
Tim Plunkett <git@plnktt.com>
2011-04-17 19:02:16 -0500

    Issue #1130092 by aspilicious, tim.plunkett: Fixed farbtastic integration and reorganized admin page.

--- fullcalendar_colors/fullcalendar_colors.admin.js
+++ fullcalendar_colors/fullcalendar_colors.admin.js
@@ -1,43 +1,20 @@
 /**
- * Implementation of hook_elements.
- *
- * NOTE: ADAPT THIS TO OUR PURPOSES (THIS IS CURRENTLY CODE FROM CALENDAR MODULE).
- * 
- * Much of the colorpicker code was adapted from the Colorpicker module.
- * That module has no stable release yet nor any D6 branch.
- */
-/*
  *  Bind the colorpicker event to the form element
  */
-(function ($) {
-Drupal.behaviors.fullcalendarColorsColorpicker = {
-  attach: function (context) {
-  // do we have multiple calendar_colors?
-  if ($("div.calendar_colorpicker").size() > 0) {
 
-    // loop over each calendar_color type
-    $("div.calendar_colorpicker").each(function() {
+jQuery(document).ready(function(){
 
-      // create the farbtastic colorpicker
-    var farb = $.farbtastic(this);
+  var farb = jQuery.farbtastic('#colorpicker');
 
-    // get the id of the current matched colorpicker wrapper div
-    var id = $(this).attr("id");
-
-    // get the calendar_color_textfields associated with this calendar_color
-    $("input.calendar_colorfield").filter("." + id).each(function () {
+  // loop over each calendar_color type
+  jQuery(".colorpicker-input").each(function() {
+ 
       // set the background colors of all of the textfields appropriately
-       farb.linkTo(this);
+      farb.linkTo(this);
 
       // when clicked, they get linked to the farbtastic colorpicker that they are associated with
-      $(this).click(function () {
+      jQuery(this).click(function () {
         farb.linkTo(this);
-      });
-
-    });
-
-    });
-  }
-  }
-};
-})(jQuery);
+	  });
+  });
+});
