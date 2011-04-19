9c34f71c940dbd25ef9f2457f13cdf38443893f0
Tim Plunkett <git@plnktt.com>
2011-04-19 17:39:05 -0500

    Issue #1130092 by tim.plunkett: Fixed some more farbtastic stuff.

--- fullcalendar_colors/fullcalendar_colors.admin.js
+++ fullcalendar_colors/fullcalendar_colors.admin.js
@@ -1,20 +1,27 @@
 /**
- *  Bind the colorpicker event to the form element
+ * @file
+ * Bind the colorpicker event to the FullCalendar Colors form element.
  */
 
-jQuery(document).ready(function(){
+(function ($) {
 
-  var farb = jQuery.farbtastic('#colorpicker');
+Drupal.behaviors.fullCalendarColors = {
+  attach: function() {
 
-  // loop over each calendar_color type
-  jQuery(".colorpicker-input").each(function() {
- 
-      // set the background colors of all of the textfields appropriately
+    var farb = $.farbtastic('#colorpicker');
+
+    // Loop over each calendar_color type.
+    $('.colorpicker-input').each(function() {
+
+      // Set the background colors of all of the textfields appropriately.
       farb.linkTo(this);
 
-      // when clicked, they get linked to the farbtastic colorpicker that they are associated with
-      jQuery(this).click(function () {
+      // When clicked, they get linked to the associated farbtastic colorpicker.
+      $(this).click(function () {
         farb.linkTo(this);
-	  });
-  });
-});
+      });
+    });
+  }
+};
+
+})(jQuery);
