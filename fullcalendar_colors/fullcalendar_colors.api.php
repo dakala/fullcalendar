1e9690a0c1a0666441c2a880c5f3cec199e83428
Tim Plunkett <git@plnktt.com>
2011-04-20 23:44:37 -0500

    Issue #1133494 by tim.plunkett: Abstract _fullcalendar_colors__create_css into a hook and API.

---
+++ fullcalendar_colors/fullcalendar_colors.api.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the FullCalendar Colors module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Adds the needed CSS classes and generated CSS.
+ *
+ * @param $classes
+ *   Array of CSS classes.
+ * @param $css
+ *   String containing generated CSS.
+ * @param $entity
+ *   Object representing the entity.
+ */
+function hook_fullcalendar_colors_create_css(&$classes, &$css, $entity) {
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
