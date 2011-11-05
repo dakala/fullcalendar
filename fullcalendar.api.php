50860181b35aeaf4972eb89d16c4d43b8571987f
Tim Plunkett <git@plnktt.com>
2011-11-05 17:52:45 -0400

    Issue #1252650 by tim.plunkett: Added a view-level 'editable' option.

--- fullcalendar.api.php
+++ fullcalendar.api.php
@@ -55,5 +55,37 @@ function hook_fullcalendar_droppable() {
 }
 
 /**
+ * Allows your module to affect the editability of the calendar.
+ *
+ * If any module implementing this hook returns FALSE, the value will be set to
+ * FALSE. Use hook_fullcalendar_editable_alter() to override this if necessary.
+ *
+ * @param $entity
+ *   Object representing the entity.
+ * @param $view
+ *   Object representing the view.
+ *
+ * @return
+ *   A boolean value dictating whether of not the calendar is editable.
+ */
+function hook_fullcalendar_editable($entity, $view) {
+   return _fullcalendar_update_access($entity);
+}
+
+/**
+ * Allows your module to forcibly override the editability of the calendar.
+ *
+ * @param $editable
+ *   A boolean value dictating whether of not the calendar is editable.
+ * @param $entity
+ *   Object representing the entity.
+ * @param $view
+ *   Object representing the view.
+ */
+function hook_fullcalendar_editable_alter(&$editable, $entity, $view) {
+  $editable = FALSE;
+}
+
+/**
  * @} End of "addtogroup hooks".
  */
