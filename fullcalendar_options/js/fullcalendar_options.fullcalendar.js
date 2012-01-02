d731dedd10933536fb10d9440b8250d949a9f4e0
Tim Plunkett <git@plnktt.com>
2012-01-02 15:39:20 -0500

    Issue #1388990 by tim.plunkett: Add option to change view by clicking on a day.

--- fullcalendar_options/js/fullcalendar_options.fullcalendar.js
+++ fullcalendar_options/js/fullcalendar_options.fullcalendar.js
@@ -2,7 +2,25 @@
 
 Drupal.fullcalendar.plugins.fullcalendar_options = {
   options: function (fullcalendar) {
-    return Drupal.settings.fullcalendar[fullcalendar.dom_id].fullcalendar_options;
+    var options = Drupal.settings.fullcalendar[fullcalendar.dom_id].fullcalendar_options;
+
+    if (options.dayClick) {
+      options.dayClick = function (date, allDay, jsEvent, view) {
+        // No need to change the view if it's already set.
+        if (view == options.dayClickView) {
+          return;
+        }
+
+        $('.fullcalendar', fullcalendar.$calendar)
+          .fullCalendar('gotoDate', date)
+          .fullCalendar('changeView', options.dayClickView);
+      };
+    }
+    else {
+      delete options.dayClick;
+    }
+
+    return options;
   }
 };
 
