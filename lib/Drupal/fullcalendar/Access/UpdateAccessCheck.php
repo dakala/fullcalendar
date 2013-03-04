cf0b430e569b447bc1b69573ae9c21861796a1f8
Tim Plunkett <git@plnktt.com>
2013-03-04 11:04:31 -0500

    Clean up _fullcalendar_update_access().

--- lib/Drupal/fullcalendar/Access/UpdateAccessCheck.php
+++ lib/Drupal/fullcalendar/Access/UpdateAccessCheck.php
@@ -10,6 +10,7 @@ namespace Drupal\fullcalendar\Access;
 use Drupal\Core\Access\AccessCheckInterface;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Entity\EntityInterface;
 
 /**
  * @todo.
@@ -27,17 +28,23 @@ class UpdateAccessCheck implements AccessCheckInterface {
    * Implements \Drupal\Core\Access\AccessCheckInterface::access().
    */
   public function access(Route $route, Request $request) {
-    global $user;
     $entity_type = $request->attributes->get('entity_type');
     $entity_id = $request->attributes->get('entity_id');
     $entity = entity_load($entity_type, $entity_id);
+    return $this->check($entity);
+  }
+
+  /**
+   * @todo.
+   */
+  public function check(EntityInterface $entity) {
+    global $user;
     if (!empty($entity) && ((user_access('administer content')
         || user_access('update any fullcalendar event')
         || user_access('edit any ' . $entity->bundle() . ' content')
         || (user_access('edit own ' . $entity->bundle() . ' content') && $entity->uid == $user->uid)))) {
       return TRUE;
     }
-
     return FALSE;
   }
 
