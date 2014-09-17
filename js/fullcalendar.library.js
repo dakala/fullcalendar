9c429f6ba2ef4876938a9eab821bb7502d83efde
Tim Plunkett <git@plnktt.com>
2014-09-17 16:16:24 -0700

    Indent all JS

--- js/fullcalendar.library.js
+++ js/fullcalendar.library.js
@@ -5,73 +5,75 @@
 
 (function ($) {
 
-Drupal.fullcalendar = Drupal.fullcalendar || {};
-Drupal.fullcalendar.plugins = Drupal.fullcalendar.plugins || {};
-Drupal.fullcalendar.cache = Drupal.fullcalendar.cache || {};
+  "use strict";
 
-// Alias old fullCalendar namespace.
-Drupal.fullCalendar = Drupal.fullcalendar;
+  Drupal.fullcalendar = Drupal.fullcalendar || {};
+  Drupal.fullcalendar.plugins = Drupal.fullcalendar.plugins || {};
+  Drupal.fullcalendar.cache = Drupal.fullcalendar.cache || {};
 
-Drupal.fullcalendar.fullcalendar = function (dom_id) {
-  this.dom_id = dom_id;
-  this.$calendar = $(dom_id);
-  this.$options = {};
-  this.navigate = false;
-  this.refetch = false;
+  // Alias old fullCalendar namespace.
+  Drupal.fullCalendar = Drupal.fullcalendar;
 
-  // Allow other modules to overwrite options.
-  var $plugins = Drupal.fullcalendar.plugins;
-  for (var i = 0; i < Drupal.settings.fullcalendar[dom_id].weights.length; i++) {
-    var $plugin = Drupal.settings.fullcalendar[dom_id].weights[i];
-    if ($plugins.hasOwnProperty($plugin) && $.isFunction($plugins[$plugin].options)) {
-      $.extend(this.$options, $plugins[$plugin].options(this, Drupal.settings.fullcalendar[this.dom_id]));
+  Drupal.fullcalendar.fullcalendar = function (dom_id) {
+    this.dom_id = dom_id;
+    this.$calendar = $(dom_id);
+    this.$options = {};
+    this.navigate = false;
+    this.refetch = false;
+
+    // Allow other modules to overwrite options.
+    var $plugins = Drupal.fullcalendar.plugins;
+    for (var i = 0; i < Drupal.settings.fullcalendar[dom_id].weights.length; i++) {
+      var $plugin = Drupal.settings.fullcalendar[dom_id].weights[i];
+      if ($plugins.hasOwnProperty($plugin) && $.isFunction($plugins[$plugin].options)) {
+        $.extend(this.$options, $plugins[$plugin].options(this, Drupal.settings.fullcalendar[this.dom_id]));
+      }
     }
-  }
 
-  this.$calendar.find('.fullcalendar').once().fullCalendar(this.$options);
+    this.$calendar.find('.fullcalendar').once().fullCalendar(this.$options);
 
-  $(this.$calendar).delegate('.fullcalendar-status-close', 'click', function () {
-    $(this).parent().slideUp();
-    return false;
-  });
-};
-
-Drupal.fullcalendar.fullcalendar.prototype.update = function (result) {
-  var fcStatus = $(result.dom_id).find('.fullcalendar-status');
-  if (fcStatus.is(':hidden')) {
-    fcStatus.html(result.msg).slideDown();
-  }
-  else {
-    fcStatus.effect('highlight');
-  }
-  Drupal.attachBehaviors();
-  return false;
-};
-
-/**
- * Parse Drupal events from the DOM.
- */
-Drupal.fullcalendar.fullcalendar.prototype.parseEvents = function (callback) {
-  var events = [];
-  var details = this.$calendar.find('.fullcalendar-event-details');
-  for (var i = 0; i < details.length; i++) {
-    var event = $(details[i]);
-    events.push({
-      field: event.data('field'),
-      index: event.data('index'),
-      eid: event.data('eid'),
-      entity_type: event.data('entity-type'),
-      title: event.attr('title'),
-      start: event.data('start'),
-      end: event.data('end'),
-      url: event.attr('href'),
-      allDay: (event.data('all-day') === 1),
-      className: event.data('cn'),
-      editable: (event.data('editable') === 1),
-      dom_id: this.dom_id
+    $(this.$calendar).delegate('.fullcalendar-status-close', 'click', function () {
+      $(this).parent().slideUp();
+      return false;
     });
-  }
-  callback(events);
-};
+  };
+
+  Drupal.fullcalendar.fullcalendar.prototype.update = function (result) {
+    var fcStatus = $(result.dom_id).find('.fullcalendar-status');
+    if (fcStatus.is(':hidden')) {
+      fcStatus.html(result.msg).slideDown();
+    }
+    else {
+      fcStatus.effect('highlight');
+    }
+    Drupal.attachBehaviors();
+    return false;
+  };
+
+  /**
+   * Parse Drupal events from the DOM.
+   */
+  Drupal.fullcalendar.fullcalendar.prototype.parseEvents = function (callback) {
+    var events = [];
+    var details = this.$calendar.find('.fullcalendar-event-details');
+    for (var i = 0; i < details.length; i++) {
+      var event = $(details[i]);
+      events.push({
+        field: event.data('field'),
+        index: event.data('index'),
+        eid: event.data('eid'),
+        entity_type: event.data('entity-type'),
+        title: event.attr('title'),
+        start: event.data('start'),
+        end: event.data('end'),
+        url: event.attr('href'),
+        allDay: (event.data('all-day') === 1),
+        className: event.data('cn'),
+        editable: (event.data('editable') === 1),
+        dom_id: this.dom_id
+      });
+    }
+    callback(events);
+  };
 
 }(jQuery));
--- js/fullcalendar.views.js
+++ js/fullcalendar.views.js
@@ -5,21 +5,23 @@
 
 (function ($) {
 
-Drupal.behaviors.fullcalendar = {
-  attach: function (context, settings) {
-    // Process each view and its settings.
-    for (var dom_id in settings.fullcalendar) {
-      if (settings.fullcalendar.hasOwnProperty(dom_id)) {
-        // Create a new fullcalendar object unless one exists.
-        if (typeof Drupal.fullcalendar.cache[dom_id] === "undefined") {
-          Drupal.fullcalendar.cache[dom_id] = new Drupal.fullcalendar.fullcalendar(dom_id);
+  "use strict";
+
+  Drupal.behaviors.fullcalendar = {
+    attach: function (context, settings) {
+      // Process each view and its settings.
+      for (var dom_id in settings.fullcalendar) {
+        if (settings.fullcalendar.hasOwnProperty(dom_id)) {
+          // Create a new fullcalendar object unless one exists.
+          if (typeof Drupal.fullcalendar.cache[dom_id] === "undefined") {
+            Drupal.fullcalendar.cache[dom_id] = new Drupal.fullcalendar.fullcalendar(dom_id);
+          }
         }
       }
-    }
 
-    // Trigger a window resize so that calendar will redraw itself.
-    $(window).resize();
-  }
-};
+      // Trigger a window resize so that calendar will redraw itself.
+      $(window).resize();
+    }
+  };
 
 }(jQuery));
